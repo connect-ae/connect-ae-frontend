f60ca4120a27469e44f4e283701c89f6f6141fd3
connect-ae <connectuae.com@gmail.com>
2025-04-10 01:20:12 +0500

    update grid layout in Pricing component for responsive design

--- app/(private)/pricing/page.tsx
+++ app/(private)/pricing/page.tsx
@@ -41,7 +41,7 @@ const Pricing = (props: Props) => {
         ]}
       />
       <Section background>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {packagesData.map((item: PricingPlan, index: number) => (
             <PricingCard
               slide={item}
